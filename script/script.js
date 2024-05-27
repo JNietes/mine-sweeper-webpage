7bdeefc51ff35d4232935b62d4bafa094bbef49f
Joaquin Nietes <108995566+JNietes@users.noreply.github.com>
2024-05-27 10:47:51 -0400

    Used QuerySelectorAll to create tileNL

--- script/script.js
+++ script/script.js
@@ -1,4 +1,9 @@
 
+const tileNL = document.querySelectorAll(".tile");
+const tileArr = Array.from(tileNL)
+for (let i = 0; i < tileArr.length; i++) {
+    console.log(tileArr[i].id);
+}
 function uncoverTile(id) {
     id.innerHTML = ":D";
 }
