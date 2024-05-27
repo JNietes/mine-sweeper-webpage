d90040450a73ebc5064b1ed14208ec4f589ccbf2
Joaquin Nietes <108995566+JNietes@users.noreply.github.com>
2024-05-27 18:22:34 -0400

    mineIndexSet

--- script/script.js
+++ script/script.js
@@ -1,9 +1,33 @@
 
+class tileObject {
+    constructor(id, mine, adjMines) {
+        this.id = id;
+        this.mine = mine;
+        this.adjMines = adjMines;
+    }
+}
+
 const tileNL = document.querySelectorAll(".tile");
 const tileArr = Array.from(tileNL)
+const tileObjectArr = [];
+
+const tileMap = new Map();
+
 for (let i = 0; i < tileArr.length; i++) {
-    console.log(tileArr[i].id);
+    tileObjectArr[i] = new tileObject(tileArr[i].id, false, 0);
+    tileMap.set(tileArr[i].id, tileObjectArr[i]);
 }
-function uncoverTile(id) {
-    id.innerHTML = ":D";
+
+const mineIndexSet = new Set();
+while (mineIndexSet.size < 10) {
+    mineIndexSet.add(parseInt((Math.random() * 64), 10));
+}
+
+for (const index of mineIndexSet) {
+    tileObjectArr[index].mine = true;
+}
+
+
+function uncoverTile(tile) {
+    tile.innerHTML = tileMap.get(tile.id).mine;
 }
