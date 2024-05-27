974065d285fd47ed07ba4ca3c7335b78abeaa775
Joaquin Nietes <108995566+JNietes@users.noreply.github.com>
2024-05-27 18:50:51 -0400

    Tiles uncover mine or # of adj mines

--- script/script.js
+++ script/script.js
@@ -27,7 +27,30 @@ for (const index of mineIndexSet) {
     tileObjectArr[index].mine = true;
 }
 
+const adjIndex = [-9, -8, -7, -1, 1, 7, 8, 9];
+for (let i=0; i<tileObjectArr.length; i++) {
+    console.log("i: " + i);
+    for (let j=0; j<adjIndex.length; j++) {
+        if (isValid(i+adjIndex[j], tileObjectArr)) {
+            if (tileObjectArr[i+adjIndex[j]].mine == true) {
+                tileObjectArr[i].adjMines++;
+            }
+        }
+    }
+}
+
+function isValid(index, arr) {
+    if (index > 0 && index < arr.length)
+        return true;
+    else
+        return false;
+}
 
 function uncoverTile(tile) {
-    tile.innerHTML = tileMap.get(tile.id).mine;
+    if (tileMap.get(tile.id).mine == true) {
+        tile.innerHTML = "Mine";
+    }
+    else {
+        tile.innerHTML = tileMap.get(tile.id).adjMines;
+    }  
 }
